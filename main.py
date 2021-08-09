98ecdf38fd73d82f4d2017d49a6860a993fa7bd1
BryanFranklin <bryan_guitarlittle@hotmail.com>
2021-08-09 20:39:37 -0300

    123

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ produto_volumes = {0:0.751, 1:0.0000899, 2:0.400, 3:0.29,
            4:0.200, 5:0.0035, 6:0.496, 7:0.0424,
            8:0.0544, 9:0.0319, 10:0.635, 11:0.87,
            12:0.498, 13:0.527}
-produto_preco = {0:999.9, 1:2199.12, 2:4346.99, 3:3999.9,
+produto_precos = {0:999.9, 1:2199.12, 2:4346.99, 3:3999.9,
           4:2999.9, 5:2499.9, 6:199.9, 7:308.66,
           8:429.9, 9:299.29, 10:849, 11:1199.89,
           12:1999.9, 13:3999 }
-volumes = []
 
 def cria_individuo():
     individuo = []
@@ -42,6 +41,19 @@ def calcula_volumes(populacao):
     print("\n")
     return soma_vols
 
-print(p, "\n")
-print(calcula_volume(0,p))
+def calcula_preco(indice, populacao):
+    soma_preco = 0
+    for j in range(len(populacao[indice])):
+        if populacao[indice][j] == 1:
+            soma_preco += produto_precos[j]
+    return soma_preco
+
+def calcula_precos(populacao):
+    soma_precos = {}
+    for i in range(len(populacao)):
+        soma_precos[i] = produto_precos(i, populacao)
+    print("\n")
+    return soma_precos
+
+print(p)
 print(calcula_volumes(p))
